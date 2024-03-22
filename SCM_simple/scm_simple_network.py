302cea6e8b2d32474ac01167bba2bd79891cde4e
SaraPJensen <sarapje@uio.no>
2024-03-22 07:23:52 +0100

    Done loads

--- SCM_simple/scm_simple_network.py
+++ SCM_simple/scm_simple_network.py
@@ -1,25 +1,22 @@
 import torch 
 import torch.nn as nn
 import numpy as np
-from scm_simple_dataset import scm_dataset_gen, scm_out_of_domain, scm_diff_seed
+from scm_simple_dataset import scm_dataset_gen, scm_out_of_domain, scm_diff_seed, Franke_data, super_simple
 from torch.optim import Adam
 from tqdm import tqdm
 from torch.utils.data import Dataset
 import torch.nn.init as init
 from lime import lime_tabular
+from sklearn.preprocessing import MinMaxScaler
 
 torch.manual_seed(2)
 np.random.seed(2)
 
 
 
-
 #region Dataset functions
 
-def normalize_data(inputs, targets):
-    # Flatten the input and target tensors
-    inputs = inputs.view(inputs.size(0), -1)
-    targets = targets.view(targets.size(0), -1)
+def normalise_data(inputs, targets):
 
     # Calculate mean and standard deviation for normalization
     input_mean = inputs.mean(dim=0)
@@ -28,12 +25,11 @@ def normalize_data(inputs, targets):
     target_std = targets.std(dim=0)
 
 
-    # Normalize the data
-    inputs_normalized = (inputs - input_mean) / input_std
-    targets_normalized = (targets - target_mean) / target_std
+    # Normalise the data
+    inputs_normalised = (inputs - input_mean) / input_std
+    targets_normalised = (targets - target_mean) / target_std
 
-    return inputs_normalized, targets_normalized
-
+    return inputs_normalised, targets_normalised
 
 
 
@@ -42,13 +38,42 @@ class MyDataset(Dataset):
         self.inputs = inputs
         self.targets = targets
 
-        #self.inputs, self.targets = normalize_data(inputs, targets)
+    def fit_scaling_inputs(self, scaler_inputs, training_data):
+        inputs, _ = training_data[:]
+        scaler_inputs.fit(inputs)
+        self.scaler_inputs = scaler_inputs
+        self.inputs = torch.from_numpy(self.scaler_inputs.transform(self.inputs))  #scale all the input features in the dataset, both training and test, according to the training data
+        self.inputs = self.inputs.to(torch.float32)
+
+        return self.scaler_inputs
+
+    def scale_inputs(self, trained_scaler_inputs):
+        self.scaler_inputs = trained_scaler_inputs
+        self.inputs = torch.from_numpy(self.scaler_inputs.transform(self.inputs))
+        self.inputs = self.inputs.to(torch.float32)
+
+
+
+    def fit_scaling_outputs(self, scaler_outputs, training_data):
+        _, outputs = training_data[:]
+        scaler_outputs.fit(outputs)
+        self.scaler_outputs = scaler_outputs
+        self.targets = torch.from_numpy(self.scaler_outputs.transform(self.targets))  #scale all the input features in the dataset, both training and test, according to the training data
+        self.targets = self.targets.to(torch.float32)
+
+        return self.scaler_outputs
+
+    def scale_outputs(self, trained_scaler_outputs):
+        self.scaler_outputs = trained_scaler_outputs
+        self.targets = torch.from_numpy(self.scaler_outputs.transform(self.targets))
+        self.targets = self.targets.to(torch.float32)
+    
+        
     
     def __getitem__(self, index):
         x = self.inputs[index]
         y = self.targets[index]
 
-        
         return x, y
 
     def __len__(self):
@@ -86,7 +111,6 @@ class TestModel(nn.Module):
                 init.constant_(m.bias, 0)
 
 
-
     
     def forward(self, x):
         x = self.linear_relu_stack(x)
@@ -103,20 +127,34 @@ class TestModel(nn.Module):
 #region Make dataset 
 
 n_datapoints = 3000
+input_scaler = MinMaxScaler()
+output_scaler = MinMaxScaler()
+
 
 inputs, outputs = scm_dataset_gen(n_datapoints)
 
-batch_size = 32
+#inputs, outputs = Franke_data(n_datapoints, 0)
 
+#inputs, outputs = super_simple(n_datapoints)
+
+
+batch_size = 32
 
 #Make torch dataset
 input_tensor = torch.from_numpy(inputs).float()
 output_tensor = torch.from_numpy(outputs).float()
 
+
 torch_dataset = MyDataset(input_tensor, output_tensor) 
-train_data, test_data = torch.utils.data.random_split(torch_dataset, [int(0.7*n_datapoints), int(0.3*n_datapoints)])
+train_data, val_data, test_data = torch.utils.data.random_split(torch_dataset, [int(0.7*n_datapoints), int(0.15*n_datapoints), int(0.15*n_datapoints)])
+
+
+trained_scaler_inputs = torch_dataset.fit_scaling_inputs(input_scaler, train_data)
+trained_scaler_outputs = torch_dataset.fit_scaling_outputs(output_scaler, train_data)
+
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle = True)
+val_loader = torch.utils.data.DataLoader(val_data, batch_size = 1, shuffle = False)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size = 1, shuffle = False)
 
 
@@ -128,6 +166,8 @@ diff_input_tensor = torch.from_numpy(diff_seed_inputs).float()
 diff_output_tensor = torch.from_numpy(diff_seed_targets).float()
 
 diff_seed_torch_dataset = MyDataset(diff_input_tensor, diff_output_tensor) 
+diff_seed_torch_dataset.scale_inputs(trained_scaler_inputs)
+diff_seed_torch_dataset.scale_outputs(trained_scaler_outputs)
 diff_seed_test_loader = torch.utils.data.DataLoader(diff_seed_torch_dataset, batch_size = 1, shuffle = False)
 
 
@@ -139,6 +179,8 @@ input_tensor = torch.from_numpy(ood_inputs).float()
 output_tensor = torch.from_numpy(ood_targets).float()
 
 ood_torch_dataset = MyDataset(input_tensor, output_tensor) 
+ood_torch_dataset.scale_inputs(trained_scaler_inputs)
+ood_torch_dataset.scale_outputs(trained_scaler_outputs)
 ood_test_loader = torch.utils.data.DataLoader(ood_torch_dataset, batch_size = 1, shuffle = False)
 
 #endregion
@@ -148,8 +190,8 @@ ood_test_loader = torch.utils.data.DataLoader(ood_torch_dataset, batch_size = 1,
 
 
 #Hyperparamerets
-learning_rate = 1e-3
-epochs = 500
+learning_rate = 1e-4
+epochs = 1000
 
 #Network parameters
 input_size = inputs.shape[1]
@@ -166,54 +208,90 @@ optimizer = Adam(model.parameters(), lr=learning_rate)
 
 #region Train model
 
+
+filename = f"MinMax_lr_{learning_rate}_train_test_loss"
+file = open(f"progress/{filename}.csv", "w")
+file.write("Epoch,train_loss,val_loss,test_loss,diff_seed_loss,out_of_domain_loss \n")
+file.close()
+
+
 with tqdm(range(epochs + 1), desc='Epochs', unit='epoch', leave=True, bar_format='{desc}: {percentage:3.0f}%|{bar}|{postfix}') as t:
+    best_val_loss = 5
+    best_epoch = 0
+
     for epoch in t:
 
         model.train()
         running_loss = 0 
         for inputs, targets in train_loader:
-            # Forward pass
-            pred = model(inputs) 
+            #Forward pass
+            optimizer.zero_grad()
+            pred = model(inputs)
             loss = loss_fn(pred, targets) 
-
 
             # Backward pass
             loss.backward()
             optimizer.step()
             running_loss += loss.item()*100
 
-            optimizer.zero_grad()
+            
 
         t.set_postfix(Loss=f'{(running_loss/len(train_loader)):.6f}') 
         t.refresh() # Refresh to show postfix
 
+        model.eval()
+        val_loss = 0
+
+        with torch.no_grad():
+            for val_inputs, val_targets in val_loader:
+                val_pred = model(val_inputs) 
+                loss = loss_fn(val_pred, val_targets) 
+                val_loss += loss.item()*100
+
+        current_val_loss = val_loss/len(val_loader)
+
+        file = open(f"progress/{filename}.csv", "a")
+        file.write(f"{epoch},{running_loss/len(train_loader)},{val_loss/len(val_loader)},{0},{0},{0} \n")
+        file.close()
+
+        if current_val_loss < best_val_loss:
+            best_epoch = epoch
+            best_val_loss = current_val_loss
+            train_loss_best_model = running_loss/len(train_loader) 
+            save_filename = f"saved_models/best_{filename}.pth"
+            torch.save(model, save_filename)
 
         if epoch % 100 == 0:
-            model.eval()
-            test_loss = 0
-
-            with torch.no_grad():
-                for inputs, targets in test_loader:
-                    pred = model(inputs) 
-                    # print(pred)
-                    # input()
-                    loss = loss_fn(pred, targets) 
-                    test_loss += loss.item()*100
 
             print(f'Epoch {epoch+1}/{epochs} \nTraining Loss: {running_loss/len(train_loader):.4f} '
-            f'\nTest Loss: {test_loss/len(test_loader):.6f}')
+            f'\nValidation Loss: {val_loss/len(val_loader):.6f}')
             print()
 
 print()
 print("Finished training")
-print(f'Final test Loss: {test_loss/len(test_loader):.6f}')
+print(f'Final val Loss: {val_loss/len(val_loader):.6f}')
 
 #endregion
 
 
 #region Test with other data
-print()
-print("Test with different seed")
+model = torch.load(f"saved_models/best_{filename}.pth")
+
+print("Best epoch: ", best_epoch)
+print(f'Best val loss: {best_val_loss:.6f}')
+
+model.eval()
+test_loss = 0
+
+with torch.no_grad():
+    for inputs, targets in test_loader:
+        pred = model(inputs)
+        loss = loss_fn(pred, targets) 
+        test_loss += loss.item()*100
+
+print(f'Test loss: {test_loss/len(test_loader):.6f}')
+
+#print("Test with different seed")
 model.eval()
 diff_seed_loss = 0
 
@@ -223,12 +301,10 @@ with torch.no_grad():
         loss = loss_fn(pred, targets) 
         diff_seed_loss += loss.item()*100
 
-print(f'Different seed Loss: {diff_seed_loss/len(diff_seed_test_loader):.6f}')
-print()
+print(f'Different seed loss: {diff_seed_loss/len(diff_seed_test_loader):.6f}')
 
 
-
-print("Test with out-of-domain data")
+#print("Test with out-of-domain data")
 
 model.eval()
 ood_loss = 0
@@ -240,16 +316,18 @@ with torch.no_grad():
         loss = loss_fn(pred, targets) 
         ood_loss += loss.item()*100
 
-print(f'Out of domain Loss: {ood_loss/len(ood_test_loader):.6f}')
+print(f'Out of domain loss: {ood_loss/len(ood_test_loader):.6f}')
 print()
 
+file = open(f"progress/{filename}.csv", "a")
+file.write(f"{best_epoch},{train_loss_best_model},{best_val_loss},{test_loss/len(test_loader)},{diff_seed_loss/len(diff_seed_test_loader)},{ood_loss/len(ood_test_loader)} \n")
+file.close()
 
 #endregion
 
 
 ### TO DO
-### Save model
-### Save training and test loss
+### Save model, using a validation set, to get the best model
 ### Include interventional data
 
 
--- SCM_simple/testing_normalisation.py
+++ SCM_simple/testing_normalisation.py
@@ -1,4 +1,26 @@
-import numpy as np 
+import numpy as np
+
+
+
+def normalize_data(inputs, targets):
+    # Flatten the input and target arrays
+    inputs = inputs.reshape(inputs.shape[0], -1)
+    targets = targets.reshape(targets.shape[0], -1)
+
+    # Calculate mean and standard deviation for normalization
+    input_mean = np.mean(inputs, axis=0)
+    input_std = np.std(inputs, axis=0)
+    target_mean = np.mean(targets, axis=0)
+    target_std = np.std(targets, axis=0)
+
+    # Normalize the data
+    inputs_normalized = (inputs - input_mean) / input_std
+    targets_normalized = (targets - target_mean) / target_std
+
+    return inputs_normalized, targets_normalized
+
+
+
 
 
 
@@ -7,8 +29,6 @@ def datapoint_gen(a, b, e):
     c = 5*a + d
     y1 = 3.5*a + 0.5*d 
     y2 = - 2*d + 0.2*e
-
-    #c = 20
 
     return c, d, y1, y2
 
@@ -20,6 +40,7 @@ def datapoint_gen(a, b, e):
 #Input array: 500 x 5
 #Output array: [y1, y2] -> 500 x 2 
 
+n_datapoints = 10
 
 def scm_dataset_gen(n_datapoints):
     np.random.seed(5)
@@ -38,9 +59,42 @@ def scm_dataset_gen(n_datapoints):
 
         outputs[i] = [y1, y2]
 
-    #print(inputs.shape)
     return inputs, outputs 
 
+
+
+inputs, outputs = scm_dataset_gen(n_datapoints)
+print("Original outputs")
+print(outputs)
+print()
+
+normalised_inputs, normalised_outputs = normalize_data(inputs, outputs)
+print("Normalised outputs")
+print(normalised_outputs)
+print()
+
+recalc_outputs = np.zeros((n_datapoints, 2))
+for i in range(0, n_datapoints):
+    a = normalised_inputs[i,0]
+    b = normalised_inputs[i, 1]
+    e = normalised_inputs[i, 4]
+    c, d, y1, y2 = datapoint_gen(a, b, e)
+
+    #inputs[i] = [A[i], B[i], c, d, E[i]]
+
+    recalc_outputs[i] = [y1, y2]
+
+
+print("Recalculated outputs")
+print(recalc_outputs)
+print()
+
+inp, renormalised_outputs = normalize_data(inputs, recalc_outputs)
+print("Renormalised outputs")
+print(renormalised_outputs)
+
+print("difference")
+print(renormalised_outputs-normalised_outputs)
 
 
 def scm_out_of_domain(n_out_of_domain):
@@ -83,49 +137,3 @@ def scm_diff_seed(n_diff_seed):
 
 
 
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    return term1 + term2 + term3 + term4
-
-
-def Franke_data(n_dpoints, noise):
-    np.random.seed(1234567)   #this was used for high noise
-
-    x = np.arange(0,1,1/n_dpoints)
-    y = np.arange(0,1,1/n_dpoints)
-
-    x, y = np.meshgrid(x, y)
-    z = FrankeFunction(x, y) + noise*np.random.randn(n_dpoints, n_dpoints)
-
-    x_flat = np.ravel(x)
-    y_flat = np.ravel(y)
-    z_flat = np.ravel(z)
-
-    inputs = np.column_stack((x_flat, y_flat))
-    print(inputs.shape)
-    print(z_flat.shape)
-    # exit()
-
-    return inputs, z_flat
-
-
-
-def super_simple(n_datapoints):
-     
-    A = np.random.uniform(-5, 10, size=n_datapoints)
-    B = np.random.uniform(5, 20, size=n_datapoints)
-    E = np.random.uniform(0, 15, size=n_datapoints)
- 
-    y1 = A
-    y2 = B + E
-
-    inputs = np.column_stack((A, B, E))
-    outputs = np.column_stack((y1, y2))
-
-    return inputs, outputs
-
-
-     
